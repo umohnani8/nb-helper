abaf173c73370209530a0a2e80d43f350203c859
Urvashi Mohnani <umohnani@redhat.com>
2024-03-07 10:00:01 -0500

    Fix base Containerfile
    And create github workflows
    
    Signed-off-by: Urvashi Mohnani <umohnani@redhat.com>

--- base/start-notebook.sh
+++ base/start-notebook.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+function start_process() {
+    trap stop_process TERM INT
+
+    echo "Running command: $@"
+    "$@" &
+
+    PID=$!
+    wait $PID
+    trap - TERM INT
+    wait $PID
+    STATUS=$?
+    exit $STATUS
+}
+
+function stop_process() {
+    kill -TERM $PID
+}
+
 # Load bash libraries
 SCRIPT_DIR=/opt/app-root/bin
 # Initialize notebooks arguments variable
